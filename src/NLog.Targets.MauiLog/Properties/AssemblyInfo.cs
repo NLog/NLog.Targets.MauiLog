f4eb856626b1de75f94d4c5e5d6785ea67ecbe1e
Rolf Kristensen <sweaty1@hotmail.com>
2022-11-22 23:15:00 +0100

    Version 1.0.2

---
+++ src/NLog.Targets.MauiLog/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Security;
+using NLog.Internal.Xamarin;
+
+[assembly: Preserve]    // Automatic --linkskip=NLog.Targets.MauiLog
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
