7cb8ca2dd8c16a03c346f761514b7f50f290c7a2
Rolf Kristensen <sweaty1@hotmail.com>
2023-05-20 12:08:15 +0200

    Add MauiApp2 to examples

---
+++ src/NLog.Targets.MauiLog/Config/SetupBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using NLog.Config;
+
+namespace NLog
+{
+    /// <summary>
+    /// Extension methods to setup LogFactory options
+    /// </summary>
+    public static class SetupBuilderExtensions
+    {
+        /// <summary>
+        /// Register the NLog.Web LayoutRenderers before loading NLog config
+        /// </summary>
+        public static ISetupBuilder RegisterMauiLog(this ISetupBuilder setupBuilder)
+        {
+            setupBuilder.SetupExtensions(e => e.RegisterMauiLog());
+            return setupBuilder;
+        }
+    }
+}
---
+++ src/NLog.Targets.MauiLog/Config/SetupExtensionsBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using NLog.Config;
+using NLog.Targets;
+
+namespace NLog
+{
+    /// <summary>
+    /// Extension methods to setup NLog extensions, so they are known when loading NLog LoggingConfiguration
+    /// </summary>
+    public static class SetupExtensionsBuilderExtensions
+    {
+        /// <summary>
+        /// Register the NLog.Web LayoutRenderers
+        /// </summary>
+        public static ISetupExtensionsBuilder RegisterMauiLog(this ISetupExtensionsBuilder setupBuilder)
+        {
+#if __ANDROID__
+            return setupBuilder.RegisterTarget<AndroidUtilLogTarget>("MauiLog");
+#elif __APPLE__
+            return setupBuilder.RegisterTarget<AppleOSLogTarget>("MauiLog");
+#else
+            return setupBuilder.RegisterTarget<ConsoleDebuggerTarget>("MauiLog");
+#endif
+        }
+    }
+}
