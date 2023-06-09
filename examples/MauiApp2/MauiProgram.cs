d3557066e48260b9a999909ed7f3c1620de72367
Rolf Kristensen <snakefoot@users.noreply.github.com>
2023-06-09 20:32:53 +0200

    Update MauiProgram.cs

--- examples/MauiApp2/MauiProgram.cs
+++ examples/MauiApp2/MauiProgram.cs
@@ -1,4 +1,4 @@
-﻿using MauiApp2.ViewModel;
+using MauiApp2.ViewModel;
 using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Extensions.Logging;
@@ -7,43 +7,43 @@ namespace MauiApp2;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var logger = NLog.LogManager.Setup().RegisterMauiLog()
-			.LoadConfiguration(c => c.ForLogger().FilterMinLevel(NLog.LogLevel.Debug).WriteToMauiLog()) // Alternative use LoadConfigurationFromAssemblyResource()
+    public static MauiApp CreateMauiApp()
+    {
+        var logger = NLog.LogManager.Setup().RegisterMauiLog()
+            .LoadConfiguration(c => c.ForLogger().FilterMinLevel(NLog.LogLevel.Debug).WriteToMauiLog()) // Alternative use LoadConfigurationFromAssemblyResource()
             .GetCurrentClassLogger();
 
-		try
-		{
-			var builder = MauiApp.CreateBuilder();
+        try
+        {
+            var builder = MauiApp.CreateBuilder();
 
             // Add NLog for Logging
             builder.Logging.ClearProviders();
-			builder.Logging.AddNLog();
+            builder.Logging.AddNLog();
 
             builder
-				.UseMauiApp<App>()
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				});
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-			builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+            builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
 
-			builder.Services.AddSingleton<MainPage>();
-			builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
 
 
-			builder.Services.AddTransient<DetailPage>();
-			builder.Services.AddTransient<DetailViewModel>();
+            builder.Services.AddTransient<DetailPage>();
+            builder.Services.AddTransient<DetailViewModel>();
 
-			return builder.Build();
-		}
-		catch (Exception ex)
-		{
+            return builder.Build();
+        }
+        catch (Exception ex)
+        {
             logger.Error(ex, "Stopped program because of exception");
             throw;
         }
-	}
+    }
 }
