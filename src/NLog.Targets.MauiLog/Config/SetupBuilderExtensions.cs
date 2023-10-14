13a0caa5681b08ecb45f3b215f943de67688eec7
Rolf Kristensen <snakefoot@users.noreply.github.com>
2023-10-14 13:19:39 +0200

    Introduced MauiExceptions to setup unified UnhandledExceptionEventHandler (#17)
    Credits Matt Johnson-Pint

--- src/NLog.Targets.MauiLog/Config/SetupBuilderExtensions.cs
+++ src/NLog.Targets.MauiLog/Config/SetupBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using NLog.Config;
+﻿using System;
+using NLog.Config;
 
 namespace NLog
 {
@@ -13,6 +14,20 @@ namespace NLog
         public static ISetupBuilder RegisterMauiLog(this ISetupBuilder setupBuilder)
         {
             setupBuilder.SetupExtensions(e => e.RegisterMauiLog());
+            return setupBuilder;
+        }
+
+        /// <summary>
+        /// Register the NLog.Web LayoutRenderers before loading NLog config
+        /// </summary>
+        public static ISetupBuilder RegisterMauiLog(this ISetupBuilder setupBuilder, UnhandledExceptionEventHandler unhandledException)
+        {
+            if (unhandledException is null)
+                throw new ArgumentNullException(nameof(unhandledException));
+
+            setupBuilder.SetupExtensions(e => e.RegisterMauiLog());
+            NLog.Targets.MauiLog.MauiExceptions.UnhandledException -= unhandledException;   // Avoid double registration
+            NLog.Targets.MauiLog.MauiExceptions.UnhandledException += unhandledException;
             return setupBuilder;
         }
     }
