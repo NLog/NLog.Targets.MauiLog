267452341c6e01700d1c6b3795cee8b155b1f493
Rolf Kristensen <snakefoot@users.noreply.github.com>
2026-08-09 11:05:17 +0200

    Update MauiExceptions.cs

--- src/NLog.Targets.MauiLog/Internal/MauiExceptions.cs
+++ src/NLog.Targets.MauiLog/Internal/MauiExceptions.cs
@@ -1,4 +1,4 @@
-﻿// MIT License
+// MIT License
 // 
 // Copyright (c) 2022 Matt Johnson-Pint
 //
@@ -31,31 +31,15 @@ namespace NLog.Targets.MauiLog
 
         static MauiExceptions()
         {
-            // This is the normal event expected, and should still be used.
-            // It will fire for exceptions from iOS and Mac Catalyst,
-            // and for exceptions on background threads from WinUI 3.
-
+            // General .NET unhandled-exception notification.
+            // Platform runtimes may have additional exception paths.
             AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
             {
                 UnhandledException?.Invoke(sender, args);
             };
 
-#if __APPLE__
-            // For iOS and Mac Catalyst
-            // Exceptions will flow through AppDomain.CurrentDomain.UnhandledException,
-            // but we need to set UnwindNativeCode to get it to work correctly. 
-            // 
-            // See: https://github.com/xamarin/xamarin-macios/issues/15252
-        
-            ObjCRuntime.Runtime.MarshalManagedException += (_, args) =>
-            {
-                args.ExceptionMode = ObjCRuntime.MarshalManagedExceptionMode.UnwindNativeCode;
-            };
-
-#elif __ANDROID__
-            // For Android:
-            // All exceptions will flow through Android.Runtime.AndroidEnvironment.UnhandledExceptionRaiser,
-            // and NOT through AppDomain.CurrentDomain.UnhandledException
+#if __ANDROID__
+            // Android has a separate unhandled-exception notification.
             if (!OperatingSystem.IsAndroidVersionAtLeast(21))
                 return;
 
